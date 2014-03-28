a09ebb65fa30d4fc5d2912069d940ae3fccf47d6
Joshua M <blckknight119@yahoo.com>
2014-03-28 08:31:33 -0400

    Update

--- Tunnelin Ted/gen/edu/wit/monplaisirj/R.java
+++ Tunnelin Ted/gen/edu/wit/monplaisirj/R.java
@@ -21,7 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bad1=0x7f020000;
+        public static final int dinoskull=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int oted=0x7f020003;
+        public static final int soil=0x7f020004;
+        public static final int ted=0x7f020005;
+        public static final int ted_outlined=0x7f020006;
+        public static final int ted_sprite=0x7f020007;
     }
     public static final class id {
         public static final int action_settings=0x7f080000;
--- Tunnelin Ted/src/edu/wit/monplaisirj/MainActivity.java
+++ Tunnelin Ted/src/edu/wit/monplaisirj/MainActivity.java
@@ -1,22 +1,16 @@
 package edu.wit.monplaisirj;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-
+import android.os.Bundle;
+import android.view.Window;
+ 
 public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(new GameView(this));
+    }
 }
+
